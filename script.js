fdb78e9ea153d40b35bbb0aa050c66f476b533a8
maximiliano836 <maxi10.02.00.g@gmail.com>
2023-10-02 21:22:00 -0300

    id productos

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 const productos=[
 	{
+		"id":1,
 		"name": "50994391-5",
 		"color": "#f9c2f8",
 		"precio": 674
 	},
 	{
+		"id":2,
 		"name": "13434658-2",
 		"color": "#8bf760",
 		"precio": 838
 	},
 	{
+		"id":3,
 		"name": "4610397-1",
 		"color": "#0e9e4e",
 		"precio": 257
 	},
 	{
+		"id":4,
 		"name": "1923755-9",
 		"color": "#1282aa",
 		"precio": 196
 	},
 	{
+		"id":5,
 		"name": "22590516-9",
 		"color": "#09ed86",
 		"precio": 608
