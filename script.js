111986a6b4b3f7b15aa34025cae70b5a5624b003
maximiliano836 <maxi10.02.00.g@gmail.com>
2023-10-04 19:08:13 -0300

    finalizando tarea

--- script.js
+++ script.js
@@ -32,37 +32,43 @@ const productos = [
   },
 ];
 
-const carrito = document.getElementById('carrito');
-const listaProductos = document.getElementById('listaProductos');
-const precioTotalElement = document.getElementById('preciototal');
-let precioTotal = 0; 
+const carrito = document.getElementById("carrito");
+const listaProductos = document.getElementById("listaProductos");
+const precioTotalElement = document.getElementById("preciototal");
+let precioTotal = 0;
 
-productos.forEach(producto => {
-  const li = document.createElement('li');
-  li.innerHTML = `
-    <div>
-      <img src="${producto.imagen}" style="max-width: 200px; max-height: 200px;" alt="${producto.name}">
-      <br>
-      Nombre: ${producto.name}
-      <br>
-      Precio: $${producto.precio}
-      <br>
+productos.forEach((producto) => {
+  const productoDiv = document.createElement("div");
+  productoDiv.className = "col-md-5 mb-5";
+
+  productoDiv.innerHTML = `
+  <div class="card ">
+    <img src="${producto.imagen}" class="card-img-top" style="max-width: 200px; max-height: 200px;" alt="${producto.name}">
+    <div class="card-body">
+      <h5 class="card-title">${producto.name}</h5>
+      <p class="card-text">Precio: $${producto.precio}</p>
       <button class="btn btn-primary comprar-btn" data-id="${producto.id}">Comprar</button>
     </div>
-  `;
+  </div>
+`;
 
-  const botonCompra = li.querySelector('.comprar-btn');
-  botonCompra.addEventListener('click', () => {
+  const comprarBtn = productoDiv.querySelector(".comprar-btn");
+  comprarBtn.addEventListener("click", () => {
     agregarAlCarrito(producto);
+    window.location.href = "#carrito";
   });
 
-  listaProductos.appendChild(li);
+  const listaProductos = document.getElementById("listaProductos");
+  listaProductos.appendChild(productoDiv);
 });
 
 function agregarAlCarrito(producto) {
-  const li = document.createElement('li');
-  li.textContent = `${producto.name} - Precio: $${producto.precio}`;
-  carrito.appendChild(li);
+  const carrito = document.getElementById("carrito");
+  const productoCarrito = document.createElement("div");
+  productoCarrito.innerHTML = `
+    <p>${producto.name} - Precio: $${producto.precio}</p>
+  `;
+  carrito.appendChild(productoCarrito);
 
   precioTotal += producto.precio;
   precioTotalElement.textContent = `Precio Total: $${precioTotal}`;
